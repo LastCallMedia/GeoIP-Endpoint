9b0002b913eba76f649925f825a593c16a296ba9
Rob Bayliss <rob@lastcallmedia.com>
2015-01-21 12:10:50 -0500

    Add an option to require the GeoIP database

--- src/lastcall/GeoIp2/Silex/GeoIp2ServiceProvider.php
+++ src/lastcall/GeoIp2/Silex/GeoIp2ServiceProvider.php
@@ -9,6 +9,8 @@ class GeoIp2ServiceProvider implements ServiceProviderInterface {
 
   public function register(Application $app) {
 
+    $app['geoip.db.require'] = FALSE;
+
     $app['geoip'] = $app->share(function() use ($app) {
       if($app['geoip.db'] && file_exists($app['geoip.db'])) {
         // Register the closure.
@@ -16,6 +18,9 @@ class GeoIp2ServiceProvider implements ServiceProviderInterface {
           $app['geoip']->close();
         });
         return new \GeoIp2\Database\Reader($app['geoip.db']);
+      }
+      if($app['geoip.db.require'] === TRUE) {
+        throw new \Exception("GeoIP Database is not found.");
       }
       return new \lastcall\GeoIp2\NullProvider();
     });

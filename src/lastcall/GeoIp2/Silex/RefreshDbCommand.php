6c78ede19a8115a953fafce8e74f5f7649e91302
Rob Bayliss <rob@lastcallmedia.com>
2016-10-05 16:22:14 -0400

    Update RefreshDbCommand to use Guzzle 6

--- src/lastcall/GeoIp2/Silex/RefreshDbCommand.php
+++ src/lastcall/GeoIp2/Silex/RefreshDbCommand.php
@@ -2,7 +2,7 @@
 
 namespace lastcall\GeoIp2\Silex;
 
-use Guzzle\Http\Client;
+use GuzzleHttp\Client;
 use Knp\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,20 +18,16 @@ class RefreshDbCommand extends Command {
     $client = new Client();
     $tmpfile = tempnam(sys_get_temp_dir(), 'geoip2-db');
     $output->writeln('Downloading new database ...');
-    $request = $client->get('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz');
-    $request->setResponseBody($tmpfile);
 
     try {
-      $response = $request->send();
-      if($response->isSuccessful()) {
-        $output->writeln('Download successful.  Unzipping...');
-        $app = $this->getSilexApplication();
-        exec('gunzip -c ' . escapeshellarg($tmpfile) . ' > ' . escapeshellarg($app['geoip.db']));
-        $output->writeln('Unzip successful.  Update complete.');
-        return 0;
-      }
-      $output->writeln(sprintf('There was a problem downloading the file: unspecified error'));
-      return 1;
+      $client->request('GET', 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz', [
+        'sink' => $tmpfile
+      ]);
+      $output->writeln('Download successful.  Unzipping...');
+      $app = $this->getSilexApplication();
+      exec('gunzip -c ' . escapeshellarg($tmpfile) . ' > ' . escapeshellarg($app['geoip.db']));
+      $output->writeln('Unzip successful.  Update complete.');
+      return 0;
     }
     catch(\Exception $e) {
       $output->writeln(sprintf('There was a problem downloading the file: %s', $e->getMessage()));
